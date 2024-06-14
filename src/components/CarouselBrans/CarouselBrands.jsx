dc9d8cc7694ba33f014ec8ce332e53ae6a30409b
JONAAVILA <jona_1969@hotmail.com>
2024-06-14 12:13:53 -0300

    carousel brands complete

---
+++ src/components/CarouselBrans/CarouselBrands.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import './CarouselBrands.css';
+
+const CarouselBrands = ({ images, interval = 3000, visibleImages = 1 }) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex >= images.length - visibleImages ? 0 : prevIndex + 1))
+  };
+
+  useEffect(() => {
+    const autoPlay = setInterval(goToNextSlide, interval)
+    return () => clearInterval(autoPlay)
+  }, [currentIndex, interval])
+
+  const displayedImages = images.slice(currentIndex, currentIndex + visibleImages)
+
+  return (
+    <div className="carousel">
+      <div className="slide-container">
+        {displayedImages.map((image, index) => (
+          <div key={index} className="slide">
+            <img src={image} alt={`Slide ${currentIndex + index}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CarouselBrands
+
---
+++ src/components/CarouselServices/CarouselServices.jsx
@@ -0,0 +1,1 @@
+import './CarouselServices.css';
